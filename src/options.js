d901906a2967726adcde72ee8f16fb01d5d78db3
Kevin Tong <kevin_tong100@hotmail.com>
2017-08-15 02:06:43 -0700

    added checkbox for subfolder, visual only for now

--- src/options.js
+++ src/options.js
@@ -1,22 +1,25 @@
 // Saves options to chrome.storage.sync.
 function save_options() {
-  var subfolder = document.getElementById('subfolder').value;
+  var folderCheck = document.getElementById('folderCheck').checked;
+  var folderName = document.getElementById('folderName').value;
   chrome.storage.sync.set({
-    subfolder: subfolder
+    isSubfolder: folderCheck,
+    subfolder: folderName
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
     status.textContent = 'Options saved.';
     setTimeout(function() {
       status.textContent = '';
-    }, 850);
+    }, 800);
   });
 }
 
 // Restores options state using the preferences stored in chrome.storage.
 function restore_options() {
-  chrome.storage.sync.get('subfolder', function(items) {
-    document.getElementById('subfolder').value = items.subfolder;
+  chrome.storage.sync.get(['isSubfolder', 'subfolder'], function(items) {
+    document.getElementById('folderCheck').checked = items.isSubfolder;
+    document.getElementById('folderName').value = items.subfolder;
   });
 }
 
